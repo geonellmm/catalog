c8e5923e65ec992309ee142833b09396acc9df33
Geonell Montehermoso <61318029+geonellmm@users.noreply.github.com>
2024-09-18 10:51:29 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(configuration().port);
+  await app.listen(configuration().port, '0.0.0.0');
 }
 bootstrap();
