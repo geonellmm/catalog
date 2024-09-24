cc201d417c00c82a19975f962c5e7556a4ef0f5c
geonellmm <geonell9276@gmail.com>
2024-09-24 13:07:46 +0800

    cicd setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Wait for the PostgreSQL service to be ready
-until pg_isready -h db -p 5432 -U postgres; do
+until pg_isready -h catalog3-database-1.cd226aaqkl6t.ap-southeast-1.rds.amazonaws.com -p 5432 -U postgres; do
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
